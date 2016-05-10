b9431b90a01def313bedac8259c99f4e4ab5d885
Doga Oz <doga.ozkaraca@gmail.com>
2016-05-11 01:30:22 +0300

    configuration updated with correct parameters to connect mysql database

--- PHP_SQL_Queries(DML)/dbconfig.php
+++ PHP_SQL_Queries(DML)/dbconfig.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'dogaozka_snDB');    // DB username
-define('DB_PASSWORD', 'dbTest12345');    // DB password
-define('DB_DATABASE', 'dogaozka_snDB');      // DB name
+define('DB_PASSWORD', '1dbTest12345');    // DB password
+define('DB_DATABASE', 'dogaozka_socialDB');      // DB name
 $connection = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die( "Unable to connect");
 $database = mysql_select_db(DB_DATABASE) or die( "Unable to select database");
 mysql_query("SET NAMES UTF8"); //for Turkish characters and also other unicode characters
